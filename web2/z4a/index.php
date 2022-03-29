a09ee92bcc68c653a9a325c96a195bc9144a39e4
NikitaDemyanenko1922 <90448942+NikitaDemyanenko1922@users.noreply.github.com>
2022-03-29 14:54:57 +0300

    Add files via upload

---
+++ web2/z4a/index.php
@@ -0,0 +1,21 @@
+﻿<html>
+<head>
+<title>Как проверить чекбокс (checkbox) на php - BlogGood.ru</title>
+</head>
+<body>
+<form method="post" action="">
+    <!-- ещё кие-то поля -->
+    <input type="checkbox" name="my_bloggood-ru" value="1" />
+    <input type="submit" name="ок" value="Отправить форму" />
+</form>
+<?php
+if ( $_POST['my_bloggood-ru'] == '')
+{
+echo 'не отмечено';
+}
+  else {
+        echo 'отмечено';
+       }
+?>
+</body>
+</html>
