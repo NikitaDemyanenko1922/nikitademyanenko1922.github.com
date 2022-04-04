4b064199299471966c668d670af278a02de0b700
NikitaDemyanenko1922 <90448942+NikitaDemyanenko1922@users.noreply.github.com>
2022-04-04 23:29:33 +0300

    Add files via upload

---
+++ web2/z4a/form.php
@@ -0,0 +1,56 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+<body>
+
+<?php
+if (!empty($messages)) {
+  print('<div id="messages">');
+  // Выводим все сообщения.
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+
+// Далее выводим форму отмечая элементы с ошибками классом error
+// и задавая начальные значения элементов ранее сохраненными.
+?>
+<div class="col-11 col-md-5 mx-auto">
+    <form id="Form" action="" method="POST" accept-charset="UTF-8" >
+    <div class="form-row">
+    <div class="col-12">
+      <label class="text-light form-item"><br />
+        Имя:<br />       
+        <input class="form-control form-control-md info" name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
+      </label><br />
+      </div>
+      <div class="col-12">
+      <label class="text-light form-item"><br />
+      Email:<br />     
+      <input class="form-control form-control-md info" name="email" type="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" />
+    </label><br />
+      </div>
+      <div class="col-12">
+      <label class="text-light form-item"><br />
+      Дата:<br />      
+      <input class="form-control form-control-md info" name="date" type="date" <?php if ($errors['date']) {print 'class="error"';} ?> value="<?php print $values['date']; ?>" />
+    </label><br />
+      </div>
+      <div class="col-12">
+      <label class="text-light form-item"><br />
+      Биография:<br />
+       <textarea class="form-control form-control-md info" name="bio" <?php if ($errors['bio']) {print 'class="error"';} ?> value="<?php print $values['bio']; ?>" ></textarea>
+      </label><br />
+      </div>
+      <label class="text-light form-item"><br />
+      Подпись:<br />   
+      <input id="checkbox" type="checkbox" name="check" required />
+    </label><br />
+      <label class="text-light form-item">Отправить форму: <input type="submit" value="ok" /></label>
+    </div>
+    </form>
+</div>
+</body>
+</html>
--- web2/z4a/index.php
+++ web2/z4a/index.php
@@ -1,22 +1,139 @@
-﻿<html>
-<head>
-<title>Как проверить чекбокс (checkbox) на php - BlogGood.ru</title>
-</head>
-<body>
-<form method="post" action="">
-    <!-- ещё кие-то поля -->
-    <input type="checkbox" name="my_bloggood-ru" value="1" />
-    <input type="submit" name="ок" value="Отправить форму" />
-</form>
 <?php
-print_r($_POST);
-if ( empty($_POST['my_bloggood-ru']))
-{
-echo 'не отмечено';
+/**
+ * Реализовать проверку заполнения обязательных полей формы в предыдущей
+ * с использованием Cookies, а также заполнение формы по умолчанию ранее
+ * введенными значениями.
+ */
+
+// Отправляем браузеру правильную кодировку,
+// файл index.php должен быть в кодировке UTF-8 без BOM.
+header('Content-Type: text/html; charset=UTF-8');
+
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  // Массив для временного хранения сообщений пользователю.
+  $messages = array();
+
+  // В суперглобальном массиве $_COOKIE PHP хранит все имена и значения куки текущего запроса.
+  // Выдаем сообщение об успешном сохранении.
+  if (!empty($_COOKIE['save'])) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('save', '', 100000);
+    // Если есть параметр save, то выводим сообщение пользователю.
+    $messages[] = 'Спасибо, результаты сохранены.';
+  }
+
+  // Складываем признак ошибок в массив.
+  $errors = array();
+  $errors['fio'] = !empty($_COOKIE['fio_error']);
+  $errors['email'] = !empty($_COOKIE['email_error']);
+  $errors['date'] = !empty($_COOKIE['date_error']);
+  $errors['bio'] = !empty($_COOKIE['bio_error']);
+  // TODO: аналогично все поля.
+
+  // Выдаем сообщения об ошибках.
+  if ($errors['fio']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('fio_error', '', 100000);
+    // Выводим сообщение.
+    $messages[] = '<div class="error">Заполните имя.</div>';
+  }
+  if ($errors['email']) {
+    setcookie('email_error', '', 100000);
+    $messages[] = '<div class="error">Заполните email.</div>';
+  }
+  if ($errors['date']) {
+    setcookie('date_error', '', 100000);
+    $messages[] = '<div class="error">Заполните дату.</div>';
+  }
+  if ($errors['bio']) {
+    setcookie('bio_error', '', 100000);
+    $messages[] = '<div class="error">Заполните биографию.</div>';
+  }
+  // TODO: тут выдать сообщения об ошибках в других полях.
+
+  // Складываем предыдущие значения полей в массив, если есть.
+  $values = array();
+  $values['fio'] = empty($_COOKIE['fio_value']) ? '' : $_COOKIE['fio_value'];
+  $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+  $values['date'] = empty($_COOKIE['date_value']) ? '' : $_COOKIE['date_value'];
+  $values['bio'] = empty($_COOKIE['bio_value']) ? '' : $_COOKIE['bio_value'];
+  $values['check'] = empty($_COOKIE['check_value']) ? '' : $_COOKIE['check_value'];
+  // TODO: аналогично все поля.
+
+  // Включаем содержимое файла form.php.
+  // В нем будут доступны переменные $messages, $errors и $values для вывода 
+  // сообщений, полей с ранее заполненными данными и признаками ошибок.
+  include('form.php');
 }
+// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+else {
+  // Проверяем ошибки.
+  $errors = FALSE;
+  if (empty($_POST['fio'])) {
+    // Выдаем куку на день с флажком об ошибке в поле fio.
+    setcookie('fio_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
   else {
-        echo 'отмечено';
-       }
-?>
-</body>
-</html>
+    // Сохраняем ранее введенное в форму значение на месяц.
+    setcookie('fio_value', $_POST['fio'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['email'])) {
+    setcookie('email_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('email_value', $_POST['email'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['date'])) {
+    setcookie('date_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('date_value', $_POST['date'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['bio'])) {
+    setcookie('bio_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('bio_value', $_POST['bio'], time() + 365 * 24 * 60 * 60);
+  }
+  if (empty($_POST['check'])) {
+    setcookie('check_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('check_value', $_POST['check'], time() + 365 * 24 * 60 * 60);
+  }
+
+// *************
+// TODO: тут необходимо проверить правильность заполнения всех остальных полей.
+// Сохранить в Cookie признаки ошибок и значения полей.
+// *************
+
+  if ($errors) {
+    // При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
+    header('Location: index.php');
+    exit();
+  }
+  else {
+    // Удаляем Cookies с признаками ошибок.
+    setcookie('fio_error', '', 100000);
+    setcookie('email_error', '', 100000);
+    setcookie('date_error', '', 100000);
+    setcookie('bio_error', '', 100000);;
+    // TODO: тут необходимо удалить остальные Cookies.
+  }
+
+  // Сохранение в БД.
+  // ...
+
+  // Сохраняем куку с признаком успешного сохранения.
+  setcookie('save', '1');
+
+  // Делаем перенаправление.
+  header('Location: index.php');
+}
