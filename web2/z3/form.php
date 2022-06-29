88ddd9ff23debd985fdafd43b196b54722d904b1
NikitaDemyanenko1922 <90448942+NikitaDemyanenko1922@users.noreply.github.com>
2022-06-29 18:12:10 +0300

    Add files via upload

---
+++ web2/z3/form.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+
+<html lang="ru">
+
+<head>
+    <title>Zadanie 3</title>
+</head>
+
+<body>
+    <header>
+            <div>
+                <img id="image" src="logo.png" alt="logo" />
+                <div id="name">Form</div>
+            </div>
+    </header>
+            <form action="/"
+                  method="POST">
+
+                <label>
+                  Имя:<br />
+                    <input name="name" placeholder="Imput name" autocomplete="off" />
+                </label><br />
+
+                <label>
+                    email:<br />
+                    <input name="email"  placeholder="Imput email" autocomplete="off" type="email" />
+                </label><br />
+
+                <label>
+                  Дата рождения:<br />
+                    <input name="date"
+                           value="ГГГГ-ММ-ДД"
+                           type="date" />
+                </label><br />
+
+                Пол:<br />
+                <label>
+                    <input type="radio" checked="checked"
+                           name="gender" value="1" />
+                    М
+                </label>
+                <label>
+                    <input type="radio"
+                           name="gender" value="2" />
+                    Ж
+                </label><br />
+
+                Число конечностей:<br />
+                <label>
+                    <input type="radio"
+                           name="limbs" value="1" />
+                    0
+                </label>
+                <label>
+                    <input type="radio"
+                           name="limbs" value="2" />
+                    1
+                </label>
+                <label>
+                    <input type="radio"
+                           name="limbs" value="3" />
+                    2
+                </label>
+                <label>
+                    <input type="radio"
+                           name="limbs" value="4" />
+                    3
+                </label>
+                <label>
+                    <input type="radio" checked="checked"
+                           name="limbs" value="5" />
+                    4
+                </label><br />
+
+                <label>
+                Суперспособности:
+                    <br />
+                    <select name="superpowers[]"
+                            multiple="multiple">
+                        <option value="1">Бессмертие</option>
+                        <option value="2">Прозождение сквозь стены</option>
+                        <option value="3">Левитация</option>
+                    </select>
+                </label><br />
+
+                <label>
+                    Биография:<br />
+                    <textarea name="bio"></textarea>
+                </label><br />
+
+                С контрактом ознакомлен(а):<br />
+                <label>
+                    <input type="checkbox"
+                           name="check-1" />
+                    Подпись
+                </label><br />
+
+                <input type="submit" value="Отправить" />
+            </form>
+        </div>
+    </nav>
+    <footer>
+        <div> (C) Nikita Demyanenko 2022 </div>
+    </footer>
+</body>
+
+</html>
---
+++ web2/z3/index.php
@@ -0,0 +1,83 @@
+<?php
+	header('Content-Type: text/html; charset=UTF-8');
+
+	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  	if (!empty($_GET['save'])) {
+      print('Спасибо, результаты сохранены.');
+  	}
+    include('form.php');
+ 		exit();
+	}else {
+		$errors = false;
+		$limbs = intval($_POST['limbs']);
+		$emailReg = "/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/";
+		if(empty($_POST['name'])) {
+			print('Заполните Имя. <br/>');
+			$errors = true;
+		}
+		if(empty($_POST['email'])) {
+			print('Заполните Email. <br/>');
+			$errors = true;
+		}else if (!preg_match($emailReg, $_POST['email'])) {
+      print('Некорректно введен Email. <br/>');
+      $errors = TRUE;
+    }
+		if(empty($_POST['date'])) {
+			print('Заполните Дату. <br/>');
+			$errors = true;
+		}
+		if (empty($_POST['gender'])) {
+      print('Заполните Пол. <br/>');
+      $errors = TRUE;
+    } else if ($_POST['gender'] != 1 && $_POST['gender'] != 2) {
+      print('Некорректно введен Пол. <br/>');
+      $errors = TRUE;
+    }
+		if ($limbs < 1 || $limbs > 5) {
+			print('<div style="color:red;margin: 5px;border:3px solid red;">Заполните поле Конечностей.</div> <br/>');
+      $errors = TRUE;
+    } else if ($limbs != 1 && $limbs != 2 &&$limbs != 3 &&$limbs != 4 &&$limbs != 5) {
+      print('Некорректно заполнен поле Конечностей. <br/>');
+			$errors = TRUE;
+    }
+		if(empty($_POST['bio'])) {
+			print('Заполните Биографию.');
+			$errors = TRUE;
+		}
+		if($errors) {
+			include('form.php');
+			exit();
+		}
+		
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$date = $_POST['date'];
+		$gender = $_POST['gender'];
+		$limbs = $_POST['limbs'];
+		$bio = $_POST['bio'];
+	
+		try{
+			$user = 'u47557';
+			$pass = '8106867';
+
+			$db = new PDO('mysql:host=localhost;dbname=u47557', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+      $stmt = $db->prepare("INSERT INTO Udata (name, email, date, gender, limbs, bio) VALUES (:name, :email, :date, :gender, :limbs, :bio)");
+      $stmt->bindParam(':name', $name);
+      $stmt->bindParam(':email', $email);
+      $stmt->bindParam(':date', $fate);
+      $stmt->bindParam(':gender', $gender);
+      $stmt->bindParam(':limbs', $limbs);
+      $stmt->bindParam(':bio', $bio);
+			if($stmt->execute()==false){
+  			print_r($stmt->errorCode());
+  			print_r($stmt->errorInfo());
+  			exit();
+ 			}
+		}	catch(PDOException $e){
+ 				print('Error : ' . $e->getMessage());
+  			exit();
+		}
+		print_r("Succesfully added new stuff, ");
+	}
+
+?>
